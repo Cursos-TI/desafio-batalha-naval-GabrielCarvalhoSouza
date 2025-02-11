00633a967378f73177450a5d58bc0c19c6c81de5
Gabriel Carvalho Souza <129614621+GabrielCarvalhoSouza@users.noreply.github.com>
2025-02-11 14:17:18 -0300

    Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -27,6 +27,7 @@ int main() {
 
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+    printf("\n");
     int tabuleiro2[10][10] = {
         {3, 3, 3, 3, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
@@ -48,25 +49,80 @@ int main() {
     }
 
     // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // Habilidade em cone:
+    printf("\n");
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    int cone[10][10] = {0};
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (i == 3 && j == 5) {
+                cone[i][j] = 1;
+            } else if (i == 4 && j == 4) {
+                cone[i][j] = 1;
+                cone[i][j + 1] = 1;
+                cone[i][j + 2] = 1;
+            } else if (i == 5 && j == 3) {
+                cone[i][j] = 1;
+                cone[i][j + 1] = 1;
+                cone[i][j + 2] = 1;
+                cone[i][j + 3] = 1;
+                cone[i][j + 4] = 1;
+            } else if (cone[i][j] != 1) {
+                cone[i][j] = 0;
+            }
+            printf("%d ", cone[i][j]);
+        }
+        printf("\n");
+    }
+    
+    // Habilidade em octaedro:
+    printf("\n");
+    
+    int octaedro[10][10] = {0};
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (i == 3 && j == 5) {
+                octaedro[i][j] = 1;
+            } else if (i == 4 && j == 4) {
+                octaedro[i][j] = 1;
+                octaedro[i][j + 1] = 1;
+                octaedro[i][j + 2] = 1;
+            } else if (i == 5 && j == 5) {
+                octaedro[i][j] = 1;
+            } else if (octaedro[i][j] != 1) {
+                octaedro[i][j] = 0;
+            }
+            printf("%d ", octaedro[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Habilidade em cruz:
+    printf("\n");
+    
+    int cruz[10][10] = {0};
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (i == 2 && j == 5) {
+                cruz[i][j] = 1;
+                cruz[i + 1][j] = 1;
+                cruz[i + 2][j] = 1;
+                cruz[i + 3][j] = 1;
+                cruz[i + 4][j] = 1;
+            } else if (i == 4 && j == 3) {
+                cruz[i][j] = 1;
+                cruz[i][j + 1] = 1;
+                cruz[i][j + 2] = 1; 
+                cruz[i][j + 3] = 1;
+                cruz[i][j + 4] = 1;
+            } else if (cruz[i][j] != 1) {
+                cruz[i][j] = 0;
+            }
+            printf("%d ", cruz[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
